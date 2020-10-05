2a2f1f64c3ba16ae526403c5e87a59e00401cbac
enrique <enrique@keyko.io>
2020-10-05 15:22:08 +0200

    update scala 12

--- src/main/scala/com/eruiz/api/http/route/HelloService.scala
+++ src/main/scala/com/eruiz/api/http/route/HelloService.scala
@@ -17,18 +17,25 @@
  */
 package com.eruiz.api.http.route
 
-import javax.ws.rs.Path
+import javax.ws.rs.{GET, POST, Path}
 
 import akka.http.scaladsl.model.{ContentTypes, HttpEntity}
 import akka.http.scaladsl.server.{Directives, Route}
-import io.swagger.annotations.{Api, ApiOperation}
+import io.swagger.v3.oas.annotations.enums.ParameterIn
+import io.swagger.v3.oas.annotations.media.{Content, Schema}
+import io.swagger.v3.oas.annotations.responses.ApiResponse
+import io.swagger.v3.oas.annotations.{Operation, Parameter}
 
-@Api(value = "/hello", produces = "application/json", consumes = "application/json")
 @Path("/hello")
 trait HelloService extends Directives {
 
-  @ApiOperation(value = "Return Hello greeting", notes = "", nickname = "Hello", httpMethod = "GET")
-  def helloRoute: Route = pathPrefix("hello") {
+  @GET
+  @Operation(summary = "Return Hello greeting (anonymous)", description = "Return Hello greeting for anonymous request",
+    responses = Array(
+      new ApiResponse(responseCode = "200", description = "Hello response",
+        content = Array(new Content(schema = new Schema(implementation = classOf[HelloService])))),
+      new ApiResponse(responseCode = "500", description = "Internal server error"))
+  )  def helloRoute: Route = pathPrefix("hello") {
     get {
       complete(HttpEntity(ContentTypes.`application/json`, s"""{"Say" : "name"}"""))
     }
